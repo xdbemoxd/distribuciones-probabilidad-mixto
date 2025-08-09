c8556c8a23f08ee6c0be1a6adbbd5d1c31d81c0d
JeanJedsonn <88215570+JeanJedsonn@users.noreply.github.com>
2025-08-09 19:49:37 -0400

    cambios menores

--- V_A_discreta/TareaDiscretas2.R
+++ V_A_discreta/TareaDiscretas2.R
@@ -5,15 +5,15 @@
 # -----------------------------------------------
 
 # a) Simulación de 20,000 muestras
-set.seed(1)  
+set.seed(0)  
 n_pruebas <- 50     #total de pruebas a realizar
 p_exito <- 0.85     #probabilidad de exito por prueba
-muestras <- rbinom(n = 20000, size = n_pruebas, prob = p_exito)
+n_muestras <- rbinom(n = 20000, size = n_pruebas, prob = p_exito)
 
 # b) Probabilidad de al menos 45 éxitos (P(X >= 45), 6 sumas )
-prob_simulada <- mean(muestras >= 45)               # muestras>=45 es una condicion, lo que convierte al vector muestras en un vector de 1's y 0's
-prob_teorica <- 1 - pbinom(44, n_pruebas, p_exito)  # P(X >= 45)
-                                                    # mean() calcula el promedio de los valores en un vector, en este caso, el promedio de 1's (max(1)/20000)
+prob_simulada <- mean(n_muestras >= 45)               # muestras>=45 es una condicion, lo que convierte al vector muestras en un vector de 1's y 0's
+prob_teorica <- 1 - pbinom(44, n_pruebas, p_exito)    # P(X >= 45)
+                                                      # mean() calcula el promedio de los valores en un vector, en este caso, el promedio de 1's (max(1)/20000)
 
 # Comparación
 cat("Probabilidad simulada (≥45 éxitos):", round(prob_simulada, 4), "\n")
@@ -21,7 +21,7 @@ cat("Probabilidad teórica (≥45 éxitos):",  round(prob_teorica, 4),  "\n")
 
 # c) Histograma y densidad teórica
 hist(
-   muestras,                                        #vector de valores (en este caso, pruebas exitosas)
+   n_muestras,                                        #vector de valores (en este caso, pruebas exitosas)
    breaks = seq(0, n_pruebas),                      #barras para cada valor (0=sin exito, 50 maximo de exitos)
    freq = FALSE,                                    #falso, dara la probabilidad de tener x exitos
    main = "Distribución Binomial (n=50, p=0.85)",   #titulo del plot
@@ -31,17 +31,20 @@ hist(
 )
 
 # Superponer densidad teórica
-valores_xlab <- 0:n_pruebas                         #valores del eje x
-valores_ylab <- dbinom(x_vals, n_pruebas, p_exito)  #vector con los valores para X=n
-lines(valores_xlab, 
-      valores_ylab, 
-      type = "b", 
-      col = "red", 
-      lwd = 2
-    )
+valores_xlab <- 0:n_pruebas                               #valores del eje x
+valores_ylab <- dbinom(valores_xlab, n_pruebas, p_exito)  #vector con los valores para X=n
+lines(
+  valores_xlab, 
+    valores_ylab, 
+    type = "b", 
+    col = "red", 
+    lwd = 2
+)
 
 # cuadro indicando el color de las funciones    
-legend("topright", 
-       legend = c("Simulación", "Teórica"),
-       fill = c("lightgreen", "red"))
+legend("topleft", 
+  legend = c("Simulación", "Teórica"),
+  fill = c("lightblue", "red")
+)
+
      
