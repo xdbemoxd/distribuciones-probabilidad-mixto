a00b718f6375b43fd699f12c359c0d2aaf6a3b53
JeanJedsonn <88215570+JeanJedsonn@users.noreply.github.com>
2025-08-09 19:38:45 -0400

    cambio de "muestras" a "n_muestras"

--- V_A_discreta/TareaDiscretas1.R
+++ V_A_discreta/TareaDiscretas1.R
@@ -11,7 +11,7 @@ n_muestras <- rpois(n = 10000, lambda)    # genera numeros aleatorios con distri
 
 # b) Probabilidad de exactamente 3 clientes
 prob_teorica <- dpois(3, lambda)        # Valor teórico P(X=3)=(λ^3)/(n!*e^λ)=5^3/(6*e^3)=125/(6*e^5)=0.140373895
-prob_simulada <- mean(muestras == 3)    # muestras==3 es una condicion, lo que convierte al vector muestras en un vector de 1's y 0's
+prob_simulada <- mean(n_muestras == 3)    # muestras==3 es una condicion, lo que convierte al vector muestras en un vector de 1's y 0's
                                         # mean() calcula el promedio de los valores en un vector, en este caso, el promedio de 1's (max(1)/10000)
 # Comparación
 cat("Probabilidad teórica (3 clientes):",  prob_teorica,  "\n")
@@ -19,8 +19,8 @@ cat("Probabilidad simulada (3 clientes):", prob_simulada, "\n")
 
 # c) Histograma y densidad teórica
 hist(
-  muestras,                                    #vector de valores (este caso, clientes con promedio)
-  breaks = seq(0, max(muestras)),              #barras para cada valor
+  n_muestras,                                    #vector de valores (este caso, clientes con promedio)
+  breaks = seq(0, max(n_muestras)),              #barras para cada valor
   freq = FALSE,                                #falso, dara la probabilidad de aparicion
   main = "Distribución de Poisson (λ=5)",      #titulo del plot
   xlab = "Número de clientes",                 #titulo del eje x
@@ -29,7 +29,7 @@ hist(
 )
 
 # Superponer distribucion teórica
-valores_xlab <- 0:max(muestras)               #valores del eje x
+valores_xlab <- 0:max(n_muestras)               #valores del eje x
 valores_ylab <- dpois(x_vals, lambda,0)       #vector con los valores para X=n
 
 lines(
@@ -46,4 +46,5 @@ legend("topright",
   legend = c("Simulación", "Teórica"),
   fill = c("lightblue", "red")
 )     
+
 
